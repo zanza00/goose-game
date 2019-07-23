dcb9735ec8942149673ea76709879b38dc57726a
Simone Picciani <zanza00@gmail.com>
2019-07-23 12:58:31 +0200

    first rough implementation of the game, jump is broken

--- src/main/scala/gg/Main.scala
+++ src/main/scala/gg/Main.scala
@@ -11,8 +11,12 @@ object Main extends App with Game {
 trait Game {
   type Player = String
   type Message = String
+  import scala.collection.mutable.Map
 
   var players: List[Player] = List()
+  val positions: Map[Player, Int] = Map[Player, Int]()
+  val rng = scala.util.Random
+
   def intro(): Unit = {
     println("""
         __            __
@@ -48,8 +52,33 @@ trait Game {
       case Some(player) => None
       case None => {
         this.players = this.players ::: List(user)
+        this.positions.put(user, 0)
         Some(user)
       }
+    }
+  }
+
+  private def getCurrentPosition(player: Player): Option[Int] =
+    this.positions.get(player)
+
+  private def getNewPosition(player: Player, amount: Int): Option[Int] =
+    this.getCurrentPosition(player).map(_ + amount)
+
+  private def updatePlayerPosition(player: Player, position: Int): Unit =
+    this.positions.update(player, position)
+
+  private def throwRandomDice(): (String, Int) = {
+    val rolls = Array(this.rng.nextInt(6) + 1, this.rng.nextInt(6) + 1)
+    (rolls.mkString(","), rolls.sum)
+  }
+
+  private def calculateJumps(start: Int, move: Int, jumps: Int): (Int, Int) = {
+    val newPosition = start + move
+    newPosition match {
+      case 5 | 9 | 14 | 18 | 23 | 27 => {
+        this.calculateJumps(newPosition, move, jumps + 1)
+      }
+      case _ => (newPosition, jumps)
     }
   }
 
@@ -57,7 +86,7 @@ trait Game {
     val arr = command.split("\\s+")
     arr.head match {
       case "add" => {
-        val player = arr.tail.tail(0)
+        val player = arr.tail.tail.head
         this.addPlayer(player) match {
           case None    => Some(s"$player: already existing player")
           case Some(_) => Some(s"Players: ${this.players.mkString(", ")}")
@@ -65,8 +94,72 @@ trait Game {
 
       }
       case "move" => {
+        val player = arr.tail.head
+        val move = arr.tail.tail.headOption match {
+          case None => this.throwRandomDice()
+          case Some(amount) => {
+            val sum = amount.split(",").map(_.toInt).sum
+            (amount, sum)
+          }
+        }
 
-        return Some(command)
+        val currentPosition = this.getCurrentPosition(player).getOrElse(0)
+        val newPosition = this.getNewPosition(player, move._2)
+
+        newPosition match {
+          case None =>
+            Some(
+              s"Invalid player, avaliable players are ${this.players.mkString(", ")}"
+            )
+          case Some(position) =>
+            position match {
+              case 63 => {
+                this.updatePlayerPosition(player, position)
+                Some(
+                  s"$player rolls ${move._1}. $player moves from $currentPosition to 63. $player Wins!!"
+                )
+              }
+              case x if x > 63 => {
+                val bounceAmount = position - 63
+                val bouncedPosition = 63 - bounceAmount
+
+                this.updatePlayerPosition(player, bouncedPosition)
+                Some(
+                  s"$player rolls ${move._1}. $player moves from $currentPosition to 63. $player bounces! Pippo returns to $bouncedPosition"
+                )
+              }
+
+              case 6 => {
+                this.updatePlayerPosition(player, 12)
+                Some(
+                  s"$player rolls ${move._1}. $player moves from $currentPosition to The Bridge. Pippo jumps to 12"
+                )
+              }
+              case 5 | 9 | 14 | 18 | 23 | 27 => {
+                val jumpsData = this.calculateJumps(position, move._2, 1)
+                val positionAfterJumps = jumpsData._1
+                val numberOfJumps = jumpsData._2
+                this.updatePlayerPosition(player, positionAfterJumps)
+                if (numberOfJumps == 1) {
+                  Some(
+                    s"$player rolls ${move._1}. $player moves from $currentPosition to $position, The Goose. Pippo moves again and goes to $positionAfterJumps"
+                  )
+                } else {
+                  Some(
+                    s"$player rolls ${move._1}. $player moves from $currentPosition to $position, The Goose. Pippo moves again and goes to ${positionAfterJumps - move._2}, The Goose. Pippo moves again and goes to $positionAfterJumps"
+                  )
+                }
+
+              }
+              case _ => {
+                this.updatePlayerPosition(player, position)
+                Some(
+                  s"$player rolls ${move._1}. $player moves from $currentPosition to $position."
+                )
+              }
+            }
+        }
+
       }
       case _ => None
     }
